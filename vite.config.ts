25628e75d51e9c348dab4ee48b323b438e0ac309
Lucascarvalhod <emanuellucassmaia@gmail.com>
2026-09-09 14:32:24 -0300

    instalando tailwind e shacnui com as devidas configurações

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
+
+const currentFile = fileURLToPath(import.meta.url)
+const currentDirectory = path.dirname(currentFile)
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(currentDirectory, './src'),
+    },
+  },
 })
